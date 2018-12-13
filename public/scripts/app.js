8d6a3aeb6c341cd0a52a891eb52502880ae6c887
MikeEngerer <michael.engerer@hotmail.com>
2018-12-13 22:21:59 +0000

    switched over to mongodb, tweets now save after server rs

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,21 +1,22 @@
 const charCounter = function() {
 	$(".new-tweet textarea").keyup(function(e) {
-		var len = $(this).val().length
-		$("span.counter").text(140 - len)
-		if ($("span.counter").text() < 0 && !$("span.counter").hasClass("red-text")) {
-			$("span.counter").attr("class", "counter red-text")
-		} else if ($("span.counter").text() >= 0 && $("span.counter").hasClass("red-text")) {
-			$("span.counter").attr("class", "counter")
+		let $counter = $("span.counter");
+		let len = $(this).val().length;
+		$counter.text(140 - len);
+		if ($counter.text() < 0 && !$counter.hasClass("red-text")) {
+			$counter.attr("class", "counter red-text");
+		} else if ($counter.text() >= 0 && $counter.hasClass("red-text")) {
+			$counter.attr("class", "counter");
 		}
 	})
 }
 
 const renderTweets = function(tweets) {
 	tweets.forEach(function(e) {
-		var $tweet = createTweetElement(e);
+		let $tweet = createTweetElement(e);
 		$('#tweet-container').prepend($tweet);
-	})
-}
+	});
+};
 
 const loadTweets = function() {
 		$.ajax({
@@ -23,13 +24,13 @@ const loadTweets = function() {
 			type: "GET", 
 			success: function(result) {
 				renderTweets(result)
-			}})
-}
+			}});
+};
 
 const renderNewest = function(tweets) {
-	var $tweet = createTweetElement(tweets[tweets.length - 1]);
+	let $tweet = createTweetElement(tweets[tweets.length - 1]);
 	$('#tweet-container').prepend($tweet);
-}
+};
 
 const loadNewest = function () {
 		$.ajax({
@@ -37,35 +38,50 @@ const loadNewest = function () {
 			type: "GET", 
 			success: function(result) {
 				renderNewest(result)
-			}})
+			}});
 	return result;
 }
 
 const toggleCompose = function () {
 	$(".compose").click(function() {
-		var $composeBox = $(".new-tweet")
+		let $composeBox = $(".new-tweet");
 		if ($composeBox.css("display") !== "none") {
-			$composeBox.slideUp()
+			$composeBox.slideUp();
 		} else {
-			$composeBox.slideDown()
-			$(".new-tweet textarea").focus()
+			$composeBox.slideDown();
+			$(".new-tweet textarea").focus();
 		}
-	})
-}
+	});
+};
 
 const escape = function(str) {
-  var div = document.createElement('div');
+  let div = document.createElement('div');
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
-}
+};
 
 const createTweetElement = function(data) {
-    var name = data.user.name,
+    let name = data.user.name,
     	avatar = data.user.avatars.small,
     	handle = data.user.handle,
     	text = data.content.text,
     	time = data.created_at,
-    	layout = `<article> 
+    	calcTime = (Math.floor((Date.now() - time) / 1000)),
+    	timeAgo;
+
+    if (calcTime < 60) {
+    	timeAgo = calcTime + " seconds";
+    } else if (calcTime < 3600) {
+    	timeAgo = Math.floor(calcTime / 60) + " minutes";
+    } else if (calcTime < 86400) {
+    	timeAgo = Math.floor(calcTime / 3600) + " hours";
+    } else if (calcTime < 31536000) {
+    	timeAgo = Math.floor(calcTime / 86400) + " days";
+    } else if (calcTime > 31536000) {
+    	timeAgo = Math.floor(calcTime / 31536000) + " years";
+    }
+
+    let layout = `<article> 
 	    			<header>
 	    				<img src="${escape(avatar)}" alt="avatar">
 	        			<h3>${escape(name)}</h3>
@@ -74,10 +90,15 @@ const createTweetElement = function(data) {
 	    			<p>
 	    				${escape(text)}
 	   				</p>
+	   				<div>
 	    			<footer>
-	        			<h5>${escape(time)}</h5>
+	        			<h5>${escape(timeAgo)} ago</h5>
+		        			<i class="fas fa-flag"></i>
+		        			<i class="fas fa-retweet"></i>
+		        			<i class="fas fa-thumbs-up"></i>
 	    			</footer>
-				  </article>`
+	    			</div>
+				  </article>`;
 	return layout;
 }
 
@@ -86,28 +107,30 @@ const postTweet = function() {
 		e.preventDefault();
 		let $rawText = $("textarea").val();
 		let len = $rawText.length;
-		let $error = $(".new-tweet div.error")
+		let $error = $(".new-tweet div.error");
 		if (len > 140) {
 			if ($error.css("display") === "block") {
-				$error.slideUp()
-				$error.text("Tweet length exceeds maximum value!");
-				$error.slideDown()
+				$error.slideUp(function() {
+					$error.text("Tweet length exceeds maximum value!");
+					$error.slideDown();
+				})
 			} else {
 				$error.text("Tweet length exceeds maximum value!");
-				$error.slideDown()
+				$error.slideDown();
 			}
 		} else if (len === 0) {
 			if ($error.css("display") === "block") {
-				$error.slideUp()
-				$error.text("Tweet must contain content!");
-				$error.slideDown()
+				$error.slideUp(function() {
+					$error.text("Tweet must contain content!");
+					$error.slideDown();
+				})
 			} else {
 				$error.text("Tweet must contain content!");
-				$error.slideDown()
+				$error.slideDown();
 			}
 		} else {
 			$error.slideUp()
-			var $text = $("textarea").serialize()
+			let $text = $("textarea").serialize();
 			$.ajax({
 			  	url: `/tweets/`,
 			 	type: 'POST',
@@ -115,16 +138,16 @@ const postTweet = function() {
 			 	success: function(result) {
 		 		loadNewest();
 		 	}})
-		 	$("textarea").val("")
-		 	$("span.counter").text(140)
+		 	$("textarea").val("");
+		 	$("span.counter").text(140);
 		}
-	})
-}
+	});
+};
 
 $(document).ready(function() {
 	postTweet();
 	loadTweets();
 	toggleCompose();
 	charCounter();
-})
+});
 
